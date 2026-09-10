2ed972f7aa0bbc1d4c194633557357d288f5a99b
Jana Al Abed <janaalabed0101@gmail.com>
2026-09-10 19:05:57 +0300

    fix: adjusting the skills set in the experience section

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -4,7 +4,14 @@ const projects = [
     name: "IT help desk",
     description:
       "An IT Help Desk & Ticketing Management System for managing support requests, ticket workflows, role-based access, and internal IT operations",
-    stack: ["React", "Tailwind CSS", "ASP.NET Web Api", "Entity Framework"],
+    stack: [
+      "React",
+      "Tailwind CSS",
+      "ASP.NET Web Api",
+      "Entity Framework",
+      "PostgreSQL",
+      "Swagger",
+    ],
     caseStudyURL: "/projects/helpdesk-ticketing-system",
     githubRepo:
       "https://github.com/janaalabed/it-helpdesk-ticketing-system.git",
@@ -15,7 +22,14 @@ const projects = [
     name: "Jobify",
     description:
       "A job-matching platform connecting students and job seekers with internship opportunities, while letting companies post openings and manage applications",
-    stack: ["Next.js", "React", "Tailwind CSS", "PostgreSQL"],
+    stack: [
+      "Next.js",
+      "Tailwind CSS",
+      "PostgreSQL",
+      "Supabase",
+      "jira",
+      "Git & Github",
+    ],
     liveDemo: "https://jobify-bice-five.vercel.app/",
     githubRepo: "https://github.com/janaalabed/techtalks-Jobify-teamD.git",
   },
@@ -24,7 +38,13 @@ const projects = [
     name: "OneHand",
     description:
       "A community platform for sharing, discovering, and exchanging unused items, connecting people who have things to give with those who need them.",
-    stack: ["Next.js", "React", "Tailwind CSS", "PostgreSQL"],
+    stack: [
+      "Next.js",
+      "Tailwind CSS",
+      "PostgreSQL",
+      "Supabase",
+      "Git & Github",
+    ],
     liveDemo: "https://onehand-community.vercel.app",
     githubRepo: "https://github.com/janaalabed/donation-platform.git",
   },
